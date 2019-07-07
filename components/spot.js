af65d31840557f00ffb8e06c0a90a911c5de4596
Adam Chambers <adammchambers@gmail.com>
2019-07-07 18:21:21 -0400

    🚧 Add share / map icons to spot card actions

--- components/spot.js
+++ components/spot.js
@@ -2,6 +2,10 @@ import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardMedia from '@material-ui/core/CardMedia'
+import CardActions from '@material-ui/core/CardActions'
+import IconButton from '@material-ui/core/IconButton'
+import ShareIcon from '@material-ui/icons/Share'
+import MapIcon from '@material-ui/icons/Map'
 import Rating from './rating'
 
 const useStyles = makeStyles({
@@ -35,6 +39,14 @@ function Spot(props) {
         image={props.data.image_url}
         title={props.data.name}
       />
+      <CardActions disableSpacing>
+        <IconButton aria-label="Add to favorites">
+          <ShareIcon />
+        </IconButton>
+        <IconButton aria-label="Share">
+          <MapIcon />
+        </IconButton>
+      </CardActions>
     </Card>
   )
 }
