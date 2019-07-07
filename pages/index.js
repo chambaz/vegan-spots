5cc4a3f2849097a6975083e62e7e47fd60b516f1
Adam Chambers <adammchambers@gmail.com>
2019-07-07 01:36:33 -0400

    💄 Style spot cards

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import { useEffect, useState } from 'react'
 import fetch from 'isomorphic-unfetch'
+import { makeStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Container from '@material-ui/core/Container'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardMedia from '@material-ui/core/CardMedia'
+
+const useStyles = makeStyles({
+  card: {
+    margin: '20px 0'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%'
+  }
+})
 
 function Home() {
   const [spots, setSpots] = useState([])
+  const classes = useStyles()
 
   useEffect(() => {
     fetch('http://localhost:3000/spots')
@@ -12,7 +27,7 @@ function Home() {
       .then(json => {
         setSpots(json.businesses)
       })
-  })
+  }, [])
 
   const loadingMsg = !spots || !spots.length ? <p>Finding hot spots...</p> : ''
 
@@ -22,7 +37,17 @@ function Home() {
       <Container>
         {loadingMsg}
         {spots.map((spot, index) => (
-          <li key={index}>{spot.name}</li>
+          <Card className={classes.card} key={index}>
+            <CardHeader
+              title={spot.name}
+              subheader={`${spot.location.address1}, ${spot.location.city}`}
+            />
+            <CardMedia
+              className={classes.media}
+              image={spot.image_url}
+              title={spot.name}
+            />
+          </Card>
         ))}
       </Container>
     </div>
